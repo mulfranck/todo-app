67eca310ddf142ef54c7a0769f2f6dbac0a4df4e
mulfranck <mulfranck5@gmail.com>
2022-04-25 16:28:18 +0100

    Add toggle navbar btn on smaller screens

--- src/ui.js
+++ src/ui.js
@@ -2,15 +2,21 @@ let $todo = document.querySelector('.todo')
 let $progress = document.querySelector('.in-progress');
 let $tabHeader = document.querySelectorAll('.tab-link');
 let $h3 = document.querySelectorAll('.list-container h3');
+let navIcon = document.querySelector('.nav-icon');
+
+
+
+
 
 // if the screen width on load is less than 650px hide all the headers
 window.addEventListener('load', (e) => {
-    e.currentTarget.innerWidth <= 650 &&
-    $h3.forEach(h3 => h3.style.visibility = 'hidden');
-
-    $todo.style.display = 'block';
-    $progress.style.display = 'none';
-    $tabHeader[0].classList.add('active');
+    if (e.currentTarget.innerWidth < 650) {
+        $h3.forEach(h3 => h3.style.visibility = 'hidden');
+        
+        $todo.style.display = 'block';
+        $progress.style.display = 'none';
+        $tabHeader[0].classList.add('active');
+    }
 
     // $h3.forEach(h3 => {
     //     h3.style.visibility = 'visible';
@@ -45,3 +51,17 @@ window.addEventListener('resize', e => {
         $progress.style.display = 'none'
     }
 })
+
+// change nav icon on mouse over to a cross
+navIcon.addEventListener('click', e => {
+    navIcon.firstChild.classList.remove('fa-bars');
+    navIcon.firstChild.classList.add('fa-times');
+    document.querySelector('.control-panel').classList.toggle('show-nav')
+})
+// remove the aside if a the is a click anywhere
+document.querySelector('.overlay').addEventListener('click', () => {
+    navIcon.firstChild.classList.add('fa-bars');
+    navIcon.firstChild.classList.remove('fa-times');
+    document.querySelector('.control-panel').classList.toggle('show-nav')
+} )
+
